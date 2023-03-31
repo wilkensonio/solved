d22cc184835b41cdabc56ef9ad25eb43968e649c
wilkenson <tcbwilkenson@gmail>
2023-03-31 08:10:37 -0400

    Algo merge sort

---
+++ algorithm/merge_sort.py
@@ -0,0 +1,38 @@
+x = [4, 3, 1, 8, 6, 3, 7, 9, 10, 45, 445, 89, 10]
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    print(left)
+    print(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+
+print(merge_sort(x))
