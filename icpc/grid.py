a7b16ae2b97ad1edd341ad5dc23e18a693e3b344
wilkenson <tcbwilkenson@gmail>
2023-04-06 20:24:15 -0400

    incomplete grid problem

--- icpc/grid.py
+++ icpc/grid.py
@@ -14,27 +14,29 @@ def grid_setup(matrix):
 
 def find_path(matrix):
     start = (0, 0)
-    grid, _, _ = grid_setup(matrix)
-    end = (len(grid) - 1, len(grid[0]) - 1)
-    y_max, x_max = end
+    grid, n, m = grid_setup(matrix)
+    end_y, end_x = (len(grid) - 1, len(grid[0]) - 1)
 
     visited = set([start])
     current_node = [(start, 0)]
 
     while current_node:
         node, num_moves = current_node.pop(0)
-        if node == end:
-            return num_moves
 
         max_jump = grid[node[0]][node[1]]
 
-        delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        next_node = [(node[0] + dy*max_jump, node[1] + dx*max_jump)
-                     for dy, dx in delta]
+        if node == (n - 1, m-1):
+            print(node, (n-1, m-1))
+            return num_moves
 
-        for next_node_y, next_node_x in next_node:
-            if (0 <= next_node_y <= y_max
-                and 0 <= next_node_x <= x_max
+        directions = [(node[1] + max_jump, node[0]),
+                      (node[1] - max_jump, node[0]),
+                      (node[1], node[0] + max_jump),
+                      (node[1], node[0] - max_jump)]
+
+        for next_node_y, next_node_x in directions:
+            if (0 <= next_node_y <= end_y
+                and 0 <= next_node_x <= end_x
                 and (next_node_y, next_node_x)
                     not in visited):
 
