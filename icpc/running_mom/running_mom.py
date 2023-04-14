855a685bbcf79b1f26dee2a25a387f7359df1dba
wilkenson <tcbwilkenson@gmail>
2023-04-13 21:10:44 -0400

    incomplete

--- icpc/running_mom/running_mom.py
+++ icpc/running_mom/running_mom.py
@@ -1,13 +1,13 @@
 import sys
 
 
-def parse_input(flights):
+def parse_input(flights: list):  # -> dict, list
 
     n_trips = (int)(flights[0][0])
-    check_safe = flights[n_trips+1:]
-    check_safe = [i for arr in check_safe for i in arr]
+    check_safe = [i for arr in flights[n_trips+1:] for i in arr]
 
     flight_graph = {}
+
     for dep, des in flights[1:n_trips + 1]:
         if dep not in flight_graph:
             flight_graph[dep] = [des]
@@ -17,35 +17,44 @@ def parse_input(flights):
     return flight_graph, check_safe
 
 
-def running_mom(from_parse):
+def find_safe_cities(from_parse: list):
     graph, check = parse_input(from_parse)
     visited = set()  # remember
-    visitting = set()  # cycle detected
+    visit = set()
+    safe = set()  # cycle detected
 
-    def dfs(city):
+    def dfs(city: str):
         if city in visited:
+            safe.add(city)
             return
+        if city not in visited and city in visit:
+            return
+
         visited.add(city)
-        for des in graph.get(city, []):
-            dfs(des)
-            if des in visited:
-                visitting.add(des)
+        visit.add(city)
+
+        for dest in graph.get(city, []):
+            dfs(dest)
+
         visited.remove(city)
 
     res = []
     for flight in check:
+        visit.clear()
         dfs(flight)
-        if flight in visitting:
+        if flight in safe:
             res.append(flight + ' safe')
         else:
             res.append(flight + ' trapped')
 
-    return '\n'.join(res)
+    print('\n'.join(res))
 
 
 flights = []
 
+
 for flight in sys.stdin:
+
     flights.append(flight.split())
 
-sys.stdout.write(running_mom(flights) + '\n')
+find_safe_cities(flights)
