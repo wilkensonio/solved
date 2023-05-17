31953fa33ff7d32192caec7675d9ce83e9fe6c47
Wilkenson <tcbwilkenson@gmail.com>
2023-05-17 11:47:36 -0400

    Add files via upload
    cons problem leetcode

---
+++ leetcode/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int amount = 11;
+        System.out.print(coinChange( coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+            for (int coin : coins) {
+                if (coin <= i){
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount]== amount + 1 ? -1 : dp[amount];
+    }
+}
