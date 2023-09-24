d61499ddab9910302e74061829e1b7713bc85858
Wilkenson <tcbwilkenson@gamilcom>
2023-09-24 01:30:23 -0400

    blew

--- icpc/promotion/promo.py
+++ icpc/promotion/promo.py
@@ -1,112 +1,99 @@
-# # https://open.kattis.com/problems/promotions
+#   https://open.kattis.com/problems/promotions
 
+from collections import defaultdict 
+import sys 
 
-import sys
+# topological sorting 
+def top_sort(graph): 
+    def dfs(node):
+        visited[node] = True # add current node to visited 
+        for neighbor in graph[node]: # recursively visit all neighbors of current 
+            if not visited[neighbor]:
+                dfs(neighbor)
+        topological_order.append(node) # add current node to the topological order
+     
+    n = num_emp   
+    visited = [False] * n  # keep track of vivisted node (deault false)
+    
+    topological_order = []  # sort the topological order (default empty) 
 
-# s2
-roots = set()
+    for i in range(n):  # iterate through all node in the graph 
+        if not visited[i] and i in graph:  # if node no in visited start dfs in that node 
+            dfs(i) 
+    return topological_order[::-1]
+   
+# compute promotions 
+def promotions(interval_start, interval_end, num_employee, rules): 
+    ''' interval [a, b] num_emplyee:int num_rules/precedence : int rules: tuples'''
+    graph = defaultdict(list)
+    in_degree = [0] * num_employee  # keep track of in_degrees of employes
+    
+    for a, b in rules:  # populate graph base on precedence rules   
+        graph[a].append(b)   # employee 'a' precedes employee 'b' 
+        in_degree[b] += 1  # increment in_degree of 'b'
+    
 
-
-def build_graph():
-    graph = {
-        'A': ['B', 'E'],
-        'B': ['C', 'F'],
-        'C': ['D'],
-        'D': [],
-        'E': ['F'],
-        'F': ['C'],
-        'G': ['E'],
-    }
-    return graph
-
-
-# s1 read input std
-def interval() -> [[str, str]]:
-    interval_list = []
-    for i in sys.stdin:
-        interval_list.append([i.strip()])
-    first_interval, rest = interval_list[:1], interval_list[1:]
-    fr = first_interval[0]
-    arr_fr = "".join(fr)
-    intervals = [arr_fr.split()[:2]]
-    for arr in rest:
-        intervals.append("".join(arr).split())
-    return intervals
-
-
-# s3
-def find_dist_from_roots():
-    graph = build_graph()
-    children = set()  # children nodes
-    key_node = set(graph.keys())  # all node
-    all_neigbors = []  # all neigbor node
-
-    for node in graph:
-        all_neigbors.append(graph[node])
-
-    # find all children  
-    for node in graph:
-        for neigh in all_neigbors:
-            if node in neigh:
-                children.add(node)
-    global roots
-    roots = key_node - children
-    # s4
-    depth_Emp: dict[int, set[str]] = {}
-    for root in roots:
-        for emp, depth in find_depth(root, graph).items():
-            depth_Emp.setdefault(depth, set()).add(emp)
-
-    return depth_Emp
-
-
-# s4 find depth
-def find_depth(root, graph):
-    if not root:
-        return 0
-    distance_graph = {}
-
-    stack = [(root, 0)]
-    while stack:
-        node, depth = stack.pop()
-        distance_graph[node] = depth
-        for neigh in graph[node]:
-            stack.append((neigh, depth + 1))
-    return distance_graph
-
-
-def solution():
-    intervals: [[str, str]] = interval()
-    dist_emp: dict[int, set[str]] = find_dist_from_roots()
-
-    interval_ab: [[int, int]] = []  # list of intervals
-    for interv in intervals:
-        interval_ab.append(list(map(int, interv)))
-
-    def find_num_promotion(distEmp):
-        promoted = []
-        curr_promo = 0
-        for ab in interval_ab:
-            d = 0  # dist employee set  1 0 1
-            i = 0  # access interval 1
-            promo = ab[i]  # number of promotion 3 1 4 2 0
-            Emp_to_be_promoted = len(distEmp[d])
-            while i < len(ab):
-                promotion = promo - Emp_to_be_promoted
-
-                if promotion >= 0:
-                    promo = promotion
-                    curr_promo += Emp_to_be_promoted
-                    d += 1
+    top_order = top_sort(graph)  # topological order employees -> [int]
+ 
+    # compute number of emp that is certained to be promoted for each interval [a, b]
+    def compute_promotion(interval1, interval2):
+        # emp up to num of promotions interval 1 and 2
+        guarantee = [e for e in top_order if in_degree[e] == 0] # no dependence 
+        promotion1 = top_order[:interval1]  
+        promotion2 = top_order[:interval2]
+        no_promotion = len(top_order) 
+        num_emp_to_promote1 = 0
+        num_emp_to_promote2 = 0 
+        eligible = set() # eligible may get promoted
+        
+        if interval1 < len(guarantee) and interval2 < len(guarantee):
+            pass
+        # hangle interval a promotion less than quarantee 
+        if len(guarantee) > interval1 and len(guarantee) <=interval2 :
+            eligible_emp(guarantee, eligible)  # eligible emp no promotion guarantee
+            promoted = len(guarantee) + len(eligible) # may be promted 
+            p2 = len(promotion2)
+            
+            while promoted < p2:
+                p2 -= 1
+                promotion2 = promotion2[:-1] # promo 2 num emp  is more than guarantee and eligible
+                num_emp_to_promote2 = len(promotion2) # update num of promotion for interval b
+                no_promotion = no_promotion - promoted # update no promotion 
+            print(num_emp_to_promote1, num_emp_to_promote2, no_promotion, sep='\n')
+        # if in_degree is zero emp is guarantee to be promoted
+        if len(guarantee) <= len(promotion1):
+            for i in range(len(promotion1) -1 , 0 , -1):
+                if in_degree[promotion1[i]] != 0:  
+                    promotion1 = promotion1[:-1] # remove employee that depend on others
                 else:
-                    promoted.append(curr_promo)
-                    curr_promo = 0
-                    i += 1
-                    d = 0
+                    if len(promotion1) > interval1:
+                        num_emp_to_promote1 = 0
+                    break  
 
-        return promoted
+            eligible_emp(guarantee, eligible)
+            
+            num_emp_to_promote1 = len(promotion1)
+            num_emp_to_promote2 = len(promotion2) 
+            
+            promo_left = num_emp_to_promote2 - num_emp_to_promote1
+            
+            while promo_left > len(eligible):
+                num_emp_to_promote2 -= 1
+                promo_left -= 1
+                
+            no_promotion = no_promotion - num_emp_to_promote2 
+            print(num_emp_to_promote1, num_emp_to_promote2, no_promotion, sep='\n') 
 
-    return find_num_promotion(dist_emp)
+    def eligible_emp(guarantee, eligible):
+        for emp in guarantee: # emp that are not guarantee promtion but are eligible
+            eligible.update(set(graph[emp]))
 
+    compute_promotion(interval_start, interval_end)
+    
 
-print(solution())
+# read input
+interval_start, interval_end, num_emp, num_rules = map(int, sys.stdin.readline().split())
+rules = [tuple(map(int, sys.stdin.readline().split())) for _ in range(num_rules)]
+
+promotions(interval_start, interval_end, num_emp, rules)
+ 
