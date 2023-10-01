e5ed11d67596996f1704260dccf36273011d2271
Wilkenson <tcbwilkenson@gamilcom>
2023-09-30 21:40:24 -0400

    icpc

--- icpc/brick wall /brickwall.py
+++ icpc/brick wall /brickwall.py
@@ -8,12 +8,16 @@ def brick_wall(N, c1, c2, c3, top_row):
         connection.add(c + top_row[i])
         c = c + top_row[i]
 
-    if solve(top_row, -1, N, c1, c2, c3, bricks_total, connection, 0):
+    if solve(top_row, -1, N, c1, c2, c3, bricks_total, connection, 0, {}):
         return "YES"
     return "NO"
 
 
-def solve(top_row, i, N, c1, c2, c3, brck_amt, connection, wall):
+def solve(top_row, i, N, c1, c2, c3, brck_amt, connection, wall, dp):
+    key = (i, c1, c2, c3, wall)
+    if key in dp:
+        return dp[key]
+
     if wall == brck_amt:
         return True
     if wall > brck_amt:
@@ -24,17 +28,20 @@ def solve(top_row, i, N, c1, c2, c3, brck_amt, connection, wall):
     for _ in range(N):
         if c1 > 0:
             if solve(top_row, i + 1, N, c1 - 1,
-                     c2, c3, brck_amt, connection, wall + 1):
+                     c2, c3, brck_amt, connection, wall + 1, dp):
+                dp[key] = True
                 return True
         if c3 > 0:
             if solve(top_row, i + 1, N, c1, c2,
-                     c3 - 1, brck_amt, connection, wall + 3):
+                     c3 - 1, brck_amt, connection, wall + 3, dp):
+                dp[key] = True
                 return True
         if c2 > 0:
             if solve(top_row, i + 1, N, c1,
-                     c2 - 1, c3, brck_amt, connection, wall + 2):
+                     c2 - 1, c3, brck_amt, connection, wall + 2, dp):
+                dp[key] = True
                 return True
-
+    dp[key] = False
     return False
 
 
