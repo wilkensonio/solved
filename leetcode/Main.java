99daa0056d165394aac128e35d44c1689d333a33
Wilkenson <tcbwilkenson@gamilcom>
2023-11-21 08:08:47 -0500

    leetcode package fix

--- leetcode/Main.java
+++ leetcode/Main.java
@@ -1,12 +1,12 @@
-package com.company;
+
 // https://leetcode.com/problems/coin-change/submissions/952121452/
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] coins = {1,2,5};
+        int[] coins = { 1, 2, 5 };
         int amount = 11;
-        System.out.print(coinChange( coins, amount));
+        System.out.print(coinChange(coins, amount));
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -14,12 +14,12 @@ public class Main {
         for (int i = 1; i <= amount; i++) {
             dp[i] = amount + 1;
             for (int coin : coins) {
-                if (coin <= i){
+                if (coin <= i) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
 
-        return dp[amount]== amount + 1 ? -1 : dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
