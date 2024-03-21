f58cb2d6b5be4bf8d0867926a904f5f7e8e72430
Wilkenson <wilkensoncode@gmail.com>
2024-03-21 13:05:15 -0400

    snake

--- icpc/snake_game/snake.py
+++ icpc/snake_game/snake.py
@@ -50,59 +50,30 @@ class Game:
         self._coin_flip = randint(1, 2)
         return self._coin_flip
 
-    def explore_map(self, board, end, move):
+    def bound(self, board, r, c):
+        return 0 <= r < len(board) and 0 <= c < len(board[0])
 
-        def bound(r, c):
-            return 0 <= r < len(board) and 0 <= c < len(board[0])
+    def explore_map(self, board, r, c, end, move):
+        if not self.bound(board, r, c):
+            return None
+        if (r, c) in self._trap_set:
+            return "Game Over"
 
-        rw, cl = self._position[1]
-        queue = deque([(rw, cl, move)])
-        pos = None
+        if board[r][c] == end:
+            return (r, c)
+
+        if move == 0:
+            return (r, c)
+
+        coin_val = self.flip_coin()
 
         for i in range(len(board)):
-            for c in range(len(board[0])):
-                queue.append((i, c, move))
-
-        while queue:
-            r, c, move = queue.popleft()
-            pos = (r, c)
-
-            if move == 0:
-                return (r, c)
-
-            # if curr player in trap do
-
-            if pos in self._trap_set:
-                return "game Over"
-
-            pos = pos
-            deltas = [(0, 1), (0, 2)]
-            coin = self.flip_coin()
-
-            for delta_r, delta_c in deltas:
-                rrow = r + delta_r
-                ccol = c + delta_c
-                is_bound = bound(rrow, ccol)
-
-                if is_bound:
-                    print(board[rrow][ccol])
-                pos = (rrow, ccol)
-                #  head of snake
-
-                if (is_bound and pos not in self._ladder_bottom
-                        and board[rrow][ccol] != end):
-                    queue.append(
-                        (rrow, ccol, move - 1)) if coin == 1 else queue.append((rrow, c + 3, move - 1))
-
-                elif (is_bound and pos and pos not in self._snake_head
-                      and board[rrow][ccol] != end and pos == self._ladder_pos.get(pos)):
-                    new_r_pos, new_c_pos = self._ladder_pos[(r, c)]
-                    queue.append((new_r_pos, new_c_pos, move - 1))
-
-                elif (rrow, ccol) in self._snake_head:
-                    snake_tail_r, snake_tail_c = self._snake_head[(
-                        rrow, ccol)]
-                    queue.append((snake_tail_r, snake_tail_c, move - 1))
+            for j in range(len(board[0])):
+                if coin_val == 1:
+                    self.explore_map(board, i, j + 1, end, move - 1)
+                    self.explore_map(board, i, j + 2, end, move - 2)
+                else:
+                    self.explore_map(board, i, j + 3, end, move - 3)
 
     def play(self, board_size: int, snakes: list, ladders: list, traps: list, move: int):
 
@@ -119,7 +90,8 @@ class Game:
             self._ladder_pos[self._position[ladder.bottom]
                              ] = self._position[ladder.top]
 
-        return self.explore_map(board=board, end=end, move=move)
+        row, col = self._position[1]
+        return self.explore_map(board, row, col, end, move)
 
 
 game = Game()
